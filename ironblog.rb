c82e10e01b0b119196d6e808ba760e76ed9fa115
Nacho Vidri <nacho@pompeiibrand.com>
2016-10-16 19:45:38 +0200

    advanced blog

--- ironblog.rb
+++ ironblog.rb
@@ -17,9 +17,13 @@ class Blog
 
 	def print_front_page
 		@list_of_posts.each do |post|
-			puts post.post_title
-			puts "**************"
-			puts "#{post.post_date}\n\n"
+			if post.sponsored == false
+				puts post.post_title
+			else
+				puts "****#{post.post_title}****"
+			end
+				puts "**************"
+				puts "#{post.post_date}\n\n"
 		end
 	end
 
@@ -28,24 +32,26 @@ class Blog
 		@list_of_posts.sort! { |post1, post2| post1.post_date <=> post2.post_date }
 		# @list_of_posts.sort! { |post| post.post_date }
 	end
+
 end
 
 
 
 class Post
 
-	attr_accessor :post_title, :post_date, :post_text
+	attr_accessor :post_title, :post_date, :post_text, :sponsored
 
-	def initialize title, date, text
+	def initialize title, date, text, sponsored = false
 		@post_title = title
 		@post_date = date
 		@post_text = text
+		@sponsored = sponsored
 	end
 end
 
 
 post1 = Post.new "Title1", "20160305", "Text1"
-post2 = Post.new "Title2", "20160110", "Text2"
+post2 = Post.new "Title2", "20160110", "Text2", true
 post3 = Post.new "Title3", "20160207", "Text3"
 
 blog1 = Blog.new
